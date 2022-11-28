d611f8a101f37d204dc4286cc06e9f6c8c7428c5
DemetriusJayme <115664765+DemetriusJayme@users.noreply.github.com>
2022-11-28 17:18:49 -0300

    segunda

---
+++ index.js
@@ -0,0 +1,103 @@
+//ARQUIVO PRINCIPAL
+import express from "express";
+import * as dotenv from "dotenv";
+//import { uuid } from "uuidv4";
+
+//habilitar o servidor a ter variáveis de ambiente
+dotenv.config();
+
+//instanciar a variável que vai ficar responsável pelo nosso servidor -> app
+const app = express();
+
+//configurar o servidor para aceitar enviar e receber arquivos em JSON
+app.use(express.json());
+
+//banco de dados
+const bancoDados = [
+  {
+    id: "e27ab2b1-cb91-4b18-ab90-5895cc9abd29",
+    documentName: "Licitação Enap - Curso Web Dev",
+    status: "Em andamento",
+    details:
+      "Processo para capacitação de servidores públicos em desenvolvimento de aplicações na WEB. Parceria com Ironhack",
+    dateInit: "28/11/2022",
+    comments: [
+      "Processo aberto",
+      "Processo partiu para as partes assinarem",
+      "Processo agora está em análise final",
+      "Processo já tem data final",
+    ],
+    dateEnd: "16/12/2022",
+    setor: "enap",
+  },
+  {
+    id: "ee5999d7-02e9-4b3d-a1ab-f067eef54173",
+    documentName: "Licitação Compras - Notebooks",
+    status: "Em andamento",
+    details: "Processo de licitação para compra de notebooks",
+    dateInit: "30/11/2022",
+    comments: ["Processo em aberto e sem previsão de conclusão"],
+    dateEnd: "",
+    setor: "tre",
+  },
+  {
+    id: "ee5999d7-02e9-4b3d-a1ab-f067eef54173",
+    documentName: "Licitação Compras - Ar Condicionado",
+    status: "Finalizado",
+    details: "Processo de licitação para compra de ar-condicionado",
+    dateInit: "15/11/2022",
+    comments: ["Processo em aberto", "Processo finalizado"],
+    dateEnd: "25/11/2022",
+    setor: "trj",
+  },
+];
+
+//CRIAÇÃO DAS ROTAS
+app.get("/all", (req, res) => {
+  // req -> request -> REQUISIÇÃO QUE VEM DO CLIENTE
+  // res -> response -> RESPOSTA PARA O CLIENTE
+
+  const bemVindo = "Bem vindo ao servidor TESTE";
+
+  //retorna uma resposta com status de 200 e um json .....
+  return res.status(200).json(bancoDados);
+});
+
+//ATIVIDADE: CRIAR UMA ROTA QUE RETORNA O BANCO DE DADOS -> ROTA -> "/all-users" verbo: GET
+app.get("/all-users", (req, res) => {
+  return res.status(200).json(bancoDados);
+});
+
+//POST - create
+app.post("/new-user", (req, res) => {
+  //console.log(req.body) // => é o CORPO da minha requisição (json)
+  //console.log(req.body.name) => apenas o nome
+
+  const form = req.body;
+
+  bancoDados.push(form);
+
+  return res.status(201).json(bancoDados);
+});
+
+//DELETE - delete a user
+app.delete("/delete/:id", (req, res) => {
+  console.log(req.params.id); // req.params -> {} por isso ele pode ser DESCONTRUÍDO
+  const { id } = req.params; // eu estou DESCONTRUINDO o req.params e ABRINDO o obj e acessando pelo NOME da chave
+
+  const deleteById = bancoDados.find((user) => user.id === id);
+  console.log(deleteById);
+  const index = bancoDados.indexOf(deleteById);
+  console.log(index);
+
+  bancoDados.splice(index, 1);
+
+  return res.status(200).json(bancoDados);
+});
+
+// o servidor subindo pro ar.
+app.listen(process.env.PORT, () => {
+  console.log(
+    `App up and running on port http://localhost:${process.env.PORT}`
+  );
+});
